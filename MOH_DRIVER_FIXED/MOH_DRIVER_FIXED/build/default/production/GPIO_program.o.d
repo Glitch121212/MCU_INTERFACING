4ed4dbe33f58af9995613443e3f251eb99d5cf60
Mohamed Hany Mohamed <mohamedhanyj1@gmail.com>
2026-04-18 11:28:48 +0200

    Delete MOH_DRIVER_FIXED directory

--- MOH_DRIVER_FIXED/MOH_DRIVER_FIXED/build/default/production/GPIO_program.o.d
+++
@@ -1,9 +0,0 @@
-build/default/production/GPIO_program.o.d \
- build/default/production/GPIO_program.o: GPIO_program.c GPIO_interface.h \
- STD_TYPES.h BIT_MATH.h
-
-GPIO_interface.h:
-
-STD_TYPES.h:
-
-BIT_MATH.h:
